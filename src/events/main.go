f5bf693d0e711665c5ee3ba31d2b0ff7bebc3e02
jadc <chehimj@gmail.com>
2024-11-19 13:33:08 -0700

    Added log for when bot is logged in

--- src/events/main.go
+++ src/events/main.go
@@ -1,10 +1,17 @@
 package events
 
 import (
+    "log"
     "github.com/bwmarrin/discordgo"
 )
 
 func RegisterAll(discord *discordgo.Session) {
+    // Log bot status
+    discord.AddHandler(func(d *discordgo.Session, r *discordgo.Ready) {
+		log.Printf("Logged in as: %v#%v", d.State.User.Username, d.State.User.Discriminator)
+	})
+
+    // Register event handlers
     // discord.AddHandler(onMessage)
     discord.AddHandler(onReaction)
     discord.AddHandler(onReactionRemove)
