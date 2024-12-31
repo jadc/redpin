21b6fa23f77fa73be0a332e78ced36d89b6b3ad5
jadc <chehimj@gmail.com>
2024-12-30 21:02:12 -0700

    Recreate webhook if last pin was too recent

--- src/misc/webhook.go
+++ src/misc/webhook.go
@@ -5,10 +5,18 @@ import (
 	"fmt"
     "log"
     "net/http"
+    "time"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/jadc/redpin/database"
 )
+
+// Hashmap of webhook id -> last used timestamp
+// Used to prevent many pins in quick succession from being merged into one message
+// Technically, if the bot is restarted, timestamps are reset and
+// messages may get merged, but this is a very unlikely scenario
+var timestamps = make(map[string]int64)
+var MERGE_TIME int64 = 10*60*1000
 
 // GetWebhook gets the webhook for a given guild
 // Returns the webhook's ID (existing or new) if successful
@@ -27,6 +35,25 @@ func GetWebhook(discord *discordgo.Session, guild_id string) (*discordgo.Webhook
     }
 
     c := db.GetConfig(guild_id)
+
+    // Discard current webhook if it is too young
+    if timestamp, ok := timestamps[webhook_id]; ok {
+        if time.Now().UnixMilli() - timestamp < MERGE_TIME {
+            log.Printf("Current webhook '%s' is too young, creating a new one", webhook_id)
+
+            // Clear outdated webhooks
+            err = discord.WebhookDelete(webhook_id)
+            if err != nil {
+                return nil, fmt.Errorf("Failed to delete outdated webhook '%s': %v", webhook_id, err)
+            }
+            delete(timestamps, webhook_id)
+
+            return createWebhook(discord, guild_id, c.Channel)
+        }
+    }
+
+    // Update timestamps
+    timestamps[webhook_id] = time.Now().UnixMilli()
 
     // Retrieve webhook object, create a new one if it's invalid
     webhook, err := discord.Webhook(webhook_id)
@@ -71,6 +98,9 @@ func createWebhook(discord *discordgo.Session, guild_id string, channel_id strin
     if err != nil {
         return nil, fmt.Errorf("Failed to add webhook to database: %v", err)
     }
+
+    // Update timestamps
+    timestamps[webhook.ID] = time.Now().UnixMilli()
 
     return webhook, nil
 }
